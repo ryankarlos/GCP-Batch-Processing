0ad775a1a94633ccd7c224745bfb039e02855276
Ryan Nazareth <ryankarlos@gmail.com>
2021-01-26 21:33:33 +0000

    refactor to put in google cloud function format

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
+def hello_world(request):
 
-import csv
-from scraper import scrape, create_url
+    import csv
+    from scraper import scrape, create_url
+    import os
+    import datetime
+    from gcloud import storage
 
-
-offset=100
-people = 1
-city = ['London', 'bristol', 'cornwall', 'brighton', 'portsmouth', 'bournemouth', 'manchester', 'yorkshire', 'gloucestershire']
-
-with open('data.csv','w') as outfile:
-    fieldnames = [
-        "name",
-        "location",
-        "price",
-        "price_for",
-        "room_type",
-        "beds",
-        "rating",
-        "rating_title",
-        "number_of_ratings",
+    max_search = os.environ("MAX_SEARCH")
+    people = os.environ("PEOPLE")
+    city = [
+        "London",
+        "bristol",
+        "cornwall",
+        "brighton",
+        "portsmouth",
+        "bournemouth",
+        "manchester",
+        "yorkshire",
+        "gloucestershire",
     ]
-    urllist = []
-    for c in city:
-        urllist.extend(create_url(people, c, offset))
+    checkin_date = datetime.datetime.now() + datetime.timedelta(os.environ("CHECKIN"))
+    checkout_date = checkin_date + datetime.timedelta(os.environ("CHECKOUT"))
 
-    writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
-    writer.writeheader()
-    for url in urllist:
-        data = scrape(url)
-        if data:
-            try:
-                for h in data['hotels']:
-                    writer.writerow(h)
-            except:
-                print("Skipping to next as offset exceeds max search")
-            # sleep(5)
+    with open("data.csv", "w") as outfile:
+        fieldnames = [
+            "name",
+            "location",
+            "price",
+            "price_for",
+            "room_type",
+            "beds",
+            "rating",
+            "rating_title",
+            "number_of_ratings",
+        ]
+        urllist = []
+        for c in city:
+            urllist.extend(
+                create_url(people, c, checkin_date, checkout_date, max_search)
+            )
+
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        for url in urllist:
+            data = scrape(url)
+            if data:
+                try:
+                    for h in data["hotels"]:
+                        writer.writerow(h)
+                except:
+                    print("Skipping to next as offset exceeds max search")
+                # sleep(5)
+
+        client = storage.Client()
+        bucket = client.get_bucket('scraping')
+        blob = bucket.blob('booking.csv')
+        blob.upload_from_filename('data.csv')
--- scraper.py
+++ scraper.py
@@ -1,34 +1,33 @@
 import requests
 from selectorlib import Extractor
-import datetime
+
 
 # Create an Extractor by reading from the YAML file
-e = Extractor.from_yaml_file('booking.yml')
+e = Extractor.from_yaml_file("booking.yml")
 
 
-today = datetime.datetime.now()
-tomorrow = today + datetime.timedelta(1)
-
-
-def create_url(people, city, offset, datein=today, dateout=tomorrow):
+def create_url(people, city, datein, dateout, offset):
 
     url_list = []
 
     for i in range(1, offset, 25):
 
-        url = "https://www.booking.com/searchresults.en-gb.html?checkin_month={in_month}" \
-            "&checkin_monthday={in_day}&checkin_year={in_year}&checkout_month={out_month}" \
-            "&checkout_monthday={out_day}&checkout_year={out_year}&group_adults={people}" \
-            "&group_children=0&order=review_score_and_price&ss={city}&offset={offset}"\
-            .format(in_month=str(datein.month),
-                    in_day=str(datein.day),
-                    in_year=str(datein.year),
-                    out_month=str(dateout.month),
-                    out_day=str(dateout.day),
-                    out_year=str(dateout.year),
-                    people=people,
-                    city=city,
-                    offset=i)
+        url = (
+            "https://www.booking.com/searchresults.en-gb.html?checkin_month={in_month}"
+            "&checkin_monthday={in_day}&checkin_year={in_year}&checkout_month={out_month}"
+            "&checkout_monthday={out_day}&checkout_year={out_year}&group_adults={people}"
+            "&group_children=0&order=review_score_and_price&ss={city}&offset={offset}".format(
+                in_month=str(datein.month),
+                in_day=str(datein.day),
+                in_year=str(datein.year),
+                out_month=str(dateout.month),
+                out_day=str(dateout.day),
+                out_year=str(dateout.year),
+                people=people,
+                city=city,
+                offset=i,
+            )
+        )
 
         url_list.append(url)
     return url_list
@@ -36,22 +35,20 @@ def create_url(people, city, offset, datein=today, dateout=tomorrow):
 
 def scrape(url):
     headers = {
-        'Connection': 'keep-alive',
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'DNT': '1',
-        'Upgrade-Insecure-Requests': '1',
+        "Connection": "keep-alive",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
         # You may want to change the user agent if you get blocked
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
-        'Referer': 'https://www.booking.com/index.en-gb.html',
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Referer": "https://www.booking.com/index.en-gb.html",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
     }
 
     # Download the page using requests
-    print("Downloading %s"%url)
+    print("Downloading %s" % url)
     r = requests.get(url, headers=headers)
     # Pass the HTML of the page and create
-    return e.extract(r.text,base_url=url)
-
+    return e.extract(r.text, base_url=url)
