9a65cc4c21c0c45c08481192b01d1d22467c02f2
Ryan Nazareth <ryankarlos@gmail.com>
2021-01-26 20:07:18 +0000

    build customised scripts building on scrapehero tutorial

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import csv
+from scraper import scrape, create_url
+
+people = 1
+city = 'London'
+
+with open("urls.txt",'r') as urllist, open('data.csv','w') as outfile:
+    fieldnames = [
+        "name",
+        "location",
+        "price",
+        "price_for",
+        "room_type",
+        "beds",
+        "rating",
+        "rating_title",
+        "number_of_ratings",
+        "url"
+    ]
+    urllist = create_url(people, city)
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+    for url in urllist:
+        data = scrape(url)
+        if data:
+            for h in data['hotels']:
+                writer.writerow(h)
+            # sleep(5)
